f0b622331d6dba7dcd0542482ec9ca04e589fd7e
Luis Angel <luanquidi1@hotmail.com>
2020-02-11 14:28:31 -0500

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+
+//Modulos
+import { SharedModule } from '../shared/shared.module';
+
+//Componentes
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+// Rutas de la Pagina
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule {}
